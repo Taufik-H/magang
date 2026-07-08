e0e3ca425b1c181bc3733a232524fe961eea822a
aisyazbiazaky <aisyazbiazaky@gmail.com>
2026-07-08 15:27:42 +0700

    belajar .length,.forEach, .map, .reduce

--- lat.js
+++ lat.js
@@ -1,5 +1,23 @@
-const colors = ['blue', 'red', 'yellow', 'green'];
+const angka = [5, 5, 5, 5, 5];
 
-for (let a = 0; a < colors.length -1; a++) {
-  console.log(colors[a]);
+for (let a = 0; a < angka.length -1; a++) {
+  console.log("length " + angka[a]);
 }
+
+angka.map((data, index) => {
+  console.log("map " + index + " " + data);
+});
+
+
+let hasil = angka.reduce((total, value) => {
+  return total + value;
+} );
+console.log("reduce " + hasil);
+
+
+const pesanan = [bakso = 10000, soto = 15000, bakmi = 20000];
+
+let totalHarga = pesanan.reduce((total, value) => {
+  return total + value;
+});
+console.log("total harga " + totalHarga);

44969c70f31ccb0ab6784fd65fe8ac540906c00b
aisyazbiazaky <aisyazbiazaky@gmail.com>
2026-07-08 17:18:40 +0700

    latihan .reduce

--- lat.js
+++ lat.js
@@ -1,6 +1,6 @@
 const angka = [5, 5, 5, 5, 5];
 
-for (let a = 0; a < angka.length -1; a++) {
+for (let a = 0; a < angka.length; a++) {
   console.log("length " + angka[a]);
 }
 
@@ -11,13 +11,17 @@ angka.map((data, index) => {
 
 let hasil = angka.reduce((total, value) => {
   return total + value;
-} );
+}, 0);
 console.log("reduce " + hasil);
 
 
-const pesanan = [bakso = 10000, soto = 15000, bakmi = 20000];
+const pesanan = [
+  { nama: "bakso", harga: 10000 },
+  { nama: "soto", harga: 15000 },
+  { nama: "bakmi", harga: 20000 }
+];
 
-let totalHarga = pesanan.reduce((total, value) => {
-  return total + value;
-});
-console.log("total harga " + totalHarga);
+let totalHarga = pesanan.reduce((total, item) => {
+  return total + item.harga;
+}, 0);
+console.log("Total Harga: " + totalHarga);
